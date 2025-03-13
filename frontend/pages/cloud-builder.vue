64679b35d7a7de99ba9c7437a73c5124b1345767
mohsen hafizi <mohsenhafizi90@gmail.com>
2025-03-13 05:49:38 -0700

    refresh

--- frontend/pages/cloud-builder.vue
+++ frontend/pages/cloud-builder.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen overflow-x-hidden">
+    <div class="min-h-screen overflow-x-hidden overflow-y-auto">
       <svg class="hidden">
 
 <symbol id="shopping-cart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
